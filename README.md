c55855b67e17fb946c29468d7d1aa9d11daba6cb
xing.wu <wuxing@bjca.org.cn>
2020-12-29 15:05:08 +0800

    更新

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
-# litadmin
-> axios请求接口的方法，一个基于基于 promise 的 HTTP 库
+# 说明
 
-## 介绍
+axios 请求接口的方法，一个基于基于 promise 的 HTTP 库
 
+# 介绍
 
-### 说明
+这是一个用 axios,QS 封装的库
 
-　这是一个用axios，QS,封装的库。
-    
 ### 项目结构
+
 ```
 ├── node_modules  #依赖包管理
 ├── index.js #封装axios的配置文件
@@ -16,68 +15,88 @@
 └── README.md #项目的说明文档，markdown 格式
 ```
 
-### 相关技术：
+### 相关说明：
 
-* axios: 基于 Promise 的 HTTP 请求客户端，可同时在浏览器和 node.js 中使用。
+- axios：基于 Promise 的 HTTP 请求客户端，可同时在浏览器和 node.js 中使用。
 
-## 项目编译和运行
+### 项目编译和运行
 
+### 1.安装 nodejs v12.16.3
 
-第一步： 先安装node v8.2.1环境，可以用nvm安装，支持多版本切换
-可参看链接：https://fengmk2.com/blog/2014/03/node-env-and-faster-npm.html
+### 2.下载项目
 
-第二步：下载项目
-先新建Vue项目，在项目里下载依赖包
+- 新建 vue 项目
+
 ```
-#npm命令下载
-npm install @xing.wu/axios --save
-```
-假定依赖包已经下载完成。
-
-第三步：引用
-（1）在VUE项目中的src文件夹下新建一个API文件夹，文件夹里面新建一个api.js
-（2）调用包
-```
-   import API from '@xing.wu/axios'
-```
-api.js里面写一些API的接口，进行接口管理
-###  接口请求方式 POSTJSON POSTFORM GET PUT DELETE PATCH
-    例如（以POSTFORM为例）： 
-```
-import API from '@xing.wu/axios';
-import { Message } from 'element-ui';
-// 错误处理
-function errorHandle(data) {
-  Message.error(data.toString());
-}
-// 处理handle
-function handle(data) {
-  if (data.status !== '0') {
-    throw Error(data.message);
-  }
-  return data.data;
-}
-API.setResponseHandle(handle);
-API.setErrorHandle(errorHandle);
-
-export default {
-  getExample: (params) => API.GET('/stastic/getCertNum', params),
-};
+npm i @xing.wu/axios
 ```
 
-### 在VUE项目的main.js调用api.js,把API的方法挂载到Vue实例上
-例如：
-```
-        import API from '../src/api/api'
-        Vue.prototype.$API = API
-```
-这样全局都可以使用这个请求方法了！！！
-### （3）调用方法
-loginParams 为请求的参数
-```
-      Vue.$API.login(loginParams).then(result => {
-        # 请求成功的操作
-      }).catch( error => {
-        # 请求失败的操作,通常可以不要这个操作，都被api中的默认处理掉了
-      })
-```
+- 使用
+
+  - 在 src 文件夹下新建 api 文件夹,新建 index.js
+  - 调用
+
+  ```
+  import API from '@xing.wu/axios';
+  ```
+
+  - 设置拦截器
+
+    请求拦截器,外部设置的优先执行
+
+    ```
+    API.interceptors.request.use(
+        (config) => {
+            ***
+            return config;
+        },
+        (error) => {
+            throw error;
+        },
+    );
+    ```
+
+    结果拦截器，外部设置的最后执行
+
+    ```
+    API.interceptors.response.use(
+      (response) => {
+        // 判断状态
+        if (response.status === '0') {
+          return response.data;
+        }
+        // 否则都是错了
+        throw new Error(JSON.stringify(response));
+      },
+      (error) => {
+        throw error;
+      },
+    );
+    ```
+
+  - 设置请求出错的配置
+
+  ```
+  // 错误处理
+  const errorHandle = (error) => {
+    // 处理错误,尝试获取error的message展示
+    try {
+      const tmp = JSON.parse(error.message);
+      const { message } = tmp;
+      Toast(message);
+    } catch (e) {
+      Toast(error.toString());
+    }
+  };
+
+  // 设置错误处理
+  API.setErrorHandle(errorHandle);
+  ```
+
+  - 请求
+
+  ```
+  export default {
+      test: (params) => API.GET('/mqc/sapi/doctor/get-doctor-info', params),
+  };
+  ```
--- index.js
+++ index.js
@@ -1,188 +1,133 @@
-/**
- * Created by sk on 2020 1 17.
- */
-import axios from 'axios'
-import QS from 'qs'
+import axios from "axios";
+import QS from "qs";
 
-//基地址
-axios.defaults.baseURL = '' //接口代理地址参见：config/index.js中的proxyTable配置
-axios.defaults.withCredentials = true
+// 默认配置
+let defaults = {
+  baseURL: "",
+  withCredentials: true,
+};
+// 写入配置
+axios.defaults = {
+  ...defaults,
+};
 
-//   是否生产环境
-const isProduction = process.env.NODE_ENV === 'production'
-// 添加业务处理函数
-let responseHandle = null
-let errorHandle = null
-// 设置业务处理函数
-function setResponseHandle (handle) {
-    responseHandle = handle
-}
-
-function setErrorHandle(handle){
-    errorHandle = handle
-}
-// 错误处理
-function interceptorErr (err) {
-    // showError(err)
-    throw err
-}
-
-function showError (err) {
-    errorHandle(err)
-}
-
-//添加一个请求拦截器
-axios.interceptors.request.use((config) => {
-    if (sessionStorage.getItem('token')) {
-        // 判断token是否存在
-        config.headers.token = sessionStorage.getItem('token') // 将token设置成请求头
+// 请求拦截器，后添加的先执行，先添加的后执行
+axios.interceptors.request.use(
+  (config) => {
+    // 判断
+    if (sessionStorage.getItem("token")) {
+      // 判断token是否存在
+      config.headers.token = sessionStorage.getItem("token"); // 将token设置成请求头
     }
-    return config
-}, interceptorErr)
+    return config;
+  },
+  (error) => {
+    // 出错
+    throw error;
+  }
+);
 
-// 添加一个响应拦截器
-axios.interceptors.response.use((response) => {
-    if (responseHandle) {
-        return responseHandle(response.data)
-    }
-    return response.data
-}, interceptorErr)
+// 响应拦截器，先添加的先执行，后添加的后执行
+axios.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    // code不为200出错
+    throw error;
+  }
+);
+let errorHandle = null;
+function setErrorHandle(handle) {
+  errorHandle = handle;
+}
 
-// let translateNetError = (error) => {
-//   return {
-//     status: error.response.status,
-//     message: '系统异常，' + error.response.statusText + '错误码：' + error.response.status,
-//   }
-// }
-//通用方法postJson
+// 创建请求,other本质是config，其中包含header等
 const POSTJSON = (url, params, others = {}) => {
-    // return axios.post(url, params).then(res => res.data)
-    return new Promise((resolve,reject) => {
-        axios
-            .post(url, params, others)
-            .then((res) => {
-                resolve(res)
-            })
-            .catch((error) => {
-                // 处理error
-                showError(error)
-                reject(error)
-                //   resolve(translateNetError(error))
-            })
-    })
-}
-
-const POSTFORM = (url, params, others = {}) => {
-    // return axios.post(url, QS.stringify(params), {headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}}).then(res => res.data)
-    return new Promise((resolve,reject) => {
-        axios
-            .post(url, QS.stringify(params), {
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                },
-                ...others,
-            })
-            .then((res) => {
-                resolve(res)
-            })
-            .catch((error) => {
-                showError(error)
-                reject(error)
-                //   resolve(translateNetError(error))
-            })
-    })
-}
+  return axios
+    .post(url, params, others)
+    .then((res) => Promise.resolve(res))
+    .catch((err) => {
+      if (errorHandle) errorHandle(err);
+      return Promise.reject(err);
+    });
+};
 
 const GET = (url, params, others = {}) => {
-    // return axios.get(url, {params: params}).then(res => res.data)
-    return new Promise((resolve,reject) => {
-        axios
-            .get(url, { params: params, ...others })
-            .then((res) => {
-                resolve(res)
-            })
-            .catch((error) => {
-                showError(error)
-                reject(error)
-                //   resolve(translateNetError(error))
-            })
+  return axios
+    .get(url, { ...others, params: params })
+    .then((res) => Promise.resolve(res))
+    .catch((err) => {
+      if (errorHandle) errorHandle(err);
+      return Promise.reject(err);
+    });
+};
+
+const POSTFORM = (url, params, others = {}) => {
+  // 这里的header设置可能存在被覆盖的风险
+  return axios
+    .post(url, QS.stringify(params), {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      },
+      ...others,
     })
-}
+    .then((res) => Promise.resolve(res))
+    .catch((err) => {
+      if (errorHandle) errorHandle(err);
+      return Promise.reject(err);
+    });
+};
 
 const PUT = (url, params) => {
-    // return axios.put(url, params).then(res => res.data)
-    return new Promise((resolve,reject) => {
-        axios
-            .put(url, params)
-            .then((res) => {
-                resolve(res)
-            })
-            .catch((error) => {
-                showError(error)
-                reject(error)
-                //   resolve(translateNetError(error))
-            })
-    })
-}
+  return axios
+    .put(url, params)
+    .then((res) => Promise.resolve(res))
+    .catch((err) => {
+      if (errorHandle) errorHandle(err);
+      return Promise.reject(err);
+    });
+};
 
 const DELETE = (url, params) => {
-    // return axios.delete(url, {params: params}).then(res => res.data)
-    return new Promise((resolve,reject) => {
-        axios
-            .delete(url, { params: params })
-            .then((res) => {
-                resolve(res)
-            })
-            .catch((error) => {
-                showError(error)
-                reject(error)
-                //   resolve(translateNetError(error))
-            })
-    })
-}
+  return axios
+    .delete(url, { params: params })
+    .then((res) => Promise.resolve(res))
+    .catch((err) => {
+      if (errorHandle) errorHandle(err);
+      return Promise.reject(err);
+    });
+};
 
 const PATCH = (url, params) => {
-    // return axios.patch(url, params).then(res => res.data)
-    return new Promise((resolve,reject) => {
-        axios
-            .patch(url, params)
-            .then((res) => {
-                resolve(res)
-            })
-            .catch((error) => {
-                showError(error)
-                reject(error)
-                //   resolve(translateNetError(error))
-            })
-    })
-}
+  return axios
+    .patch(url, params)
+    .then((res) => Promise.resolve(res))
+    .catch((err) => {
+      if (errorHandle) errorHandle(err);
+      return Promise.reject(err);
+    });
+};
 
 // 通用，可设置多属性 自定义header，以及其他参数
 const POSTJSONALL = (url, params, others = {}) => {
-    // return axios.post(url, params, others).then(res => res.data)
-    return new Promise((resolve,reject) => {
-        axios
-            .patch(url, params, others)
-            .then((res) => {
-                resolve(res)
-            })
-            .catch((error) => {
-                showError(error)
-                reject(error)
-                //   resolve(translateNetError(error))
-            })
-    })
-}
+  return axios
+    .patch(url, params, others)
+    .then((res) => Promise.resolve(res))
+    .catch((err) => {
+      if (errorHandle) errorHandle(err);
+      return Promise.reject(err);
+    });
+};
 
 export default {
-    ...axios,
-    POSTJSON,
-    POSTFORM,
-    GET,
-    PUT,
-    DELETE,
-    PATCH,
-    POSTJSONALL,
-    setResponseHandle,
-    setErrorHandle
-}
+  ...axios,
+  setErrorHandle,
+  POSTJSON,
+  POSTFORM,
+  GET,
+  PUT,
+  DELETE,
+  PATCH,
+  POSTJSONALL,
+};
